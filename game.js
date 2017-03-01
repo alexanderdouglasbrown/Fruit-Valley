5e2804648358e9f1eb6e5515e4ca5346a8d3648e
= <abrown@mail.sfsu.edu>
2017-03-01 02:13:37 -0800

    More fruit. Some changes

--- game.js
+++ game.js
@@ -1,12 +1,13 @@
 function game() {
     const ctx = JSG.context
 
+    const tileSize = 120
+    const tileOffset = 180
+
     const backgroundImage = new Image()
     const backgroundTile = new Image()
     const backgroundTile2 = new Image()
-    const fruitGrape = new Image()
-    const fruitApple = new Image()
-    const fruitOrange = new Image()
+    const fruits = new Image()
 
     let puzzleArray = null
 
@@ -18,9 +19,7 @@ function game() {
         backgroundImage.src = "graphics/forestbackground.png"
         backgroundTile.src = "graphics/tile.png"
         backgroundTile2.src = "graphics/tile2.png"
-        fruitGrape.src = "graphics/grape.png"
-        fruitApple.src = "graphics/apple.png"
-        fruitOrange.src = "graphics/orange.png"
+        fruits.src = "graphics/fruits.png"
     }
 
     JSG.internal.update = function update(dt) {
@@ -39,21 +38,13 @@ function game() {
             for (let j = 0; j < 9; j++) {
                 //Background tile
                 if ((i + j) % 2 == 0)
-                    ctx.drawImage(backgroundTile, 360 + (120 * i), 120 * j)
+                    ctx.drawImage(backgroundTile, tileOffset + (tileSize * i), tileSize * j)
                 else
-                    ctx.drawImage(backgroundTile2, 360 + (120 * i), 120 * j)
+                    ctx.drawImage(backgroundTile2, tileOffset + (tileSize * i), tileSize * j)
                 //Fruits
-                switch (puzzleArray[j][i]) {
-                    case 1:
-                        ctx.drawImage(fruitGrape, 360 + (120 * i), 120 * j)
-                        break
-                    case 2:
-                        ctx.drawImage(fruitApple, 360 + (120 * i), 120 * j)
-                        break
-                    case 3:
-                        ctx.drawImage(fruitOrange, 360 + (120 * i), 120 * j)
-                        break
-                }
+                
+                ctx.drawImage(fruits, puzzleArray[i][j] * tileSize, 0, tileSize, tileSize, tileOffset + (tileSize * i), tileSize * j, tileSize, tileSize)
+
             }
         }
     }
@@ -75,7 +66,7 @@ function game() {
 
         for (let i = 0; i < tempGrid.length; i++)
             for (let j = 0; j < tempGrid[i].length; j++)
-                tempGrid[j][i] = getRandomIntInclusive(1, 3)
+                tempGrid[j][i] = getRandomIntInclusive(1, 5)
 
 
         return tempGrid
